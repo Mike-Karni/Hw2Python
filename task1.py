64037cf081bed841c9502098c00ab6e09ced5642
mike_karni <3923499@mail.ru>
2022-07-13 18:37:57 +0300

    FirstTaskDone

--- task1.py
+++ task1.py
@@ -3,20 +3,7 @@
 - 6782 -> 23
 - 0,56 -> 11'''
 
-import main as sum
-
-
+import task2 as FunctionFile
 number = input("Введите число ")
-print(main.sum(number))
+print(FunctionFile.Sum(number))
 
-print()
-def Sum(num):
-    sum = 0
-    for i in range(len(num)):
-        if num[i].isdigit():
-            sum += int(num[i])
-    return sum
-
-number = input("Введите число ")
-
-print(Sum(number))

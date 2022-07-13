bf8dcd4cc5c06a5c44bb1256b860169d13c18660
mike_karni <3923499@mail.ru>
2022-07-13 18:32:28 +0300

    SSSS

---
+++ task1.py
@@ -0,0 +1,16 @@
+'''Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+Пример:
+- 6782 -> 23
+- 0,56 -> 11'''
+
+import main
+def Sum(num):
+    sum = 0
+    for i in range(len(num)):
+        if num[i].isdigit():
+            sum += int(num[i])
+    return sum
+
+number = input("Введите число ")
+
+print(Sum(number))

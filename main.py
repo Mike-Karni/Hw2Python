0b30e26cb972b9538c52cca1fcaae32210197d11
mike_karni <3923499@mail.ru>
2022-07-14 19:42:06 +0300

    Done

--- main.py
+++ main.py
@@ -3,26 +3,8 @@
 Пример:
  пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)'''
 
-
-'''
-factorial =1
-list= []
-for i in range(1, n + 1):
-    factorial *= i
-    list.append(factorial)
-print(list)'''
-'''
-def Factorial(n):
-    factorial = 1
-    list = []
-    for i in range(1, n + 1):
-        factorial *= i
-        list.append(factorial)
-    return list
-
-print(Factorial(n))'''
 import task2 as FunctionFile
 
-n = int(input("Введи какуюнибудь херню " ))
+n = int(input("Введи какуюнибудь число " ))
 
 print(FunctionFile.Factorial(n))
